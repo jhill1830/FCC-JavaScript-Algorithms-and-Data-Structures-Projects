eb0a18ae7550b957ca5baaa03717cc685f92390c
James <36040856+jhill1830@users.noreply.github.com>
2022-08-26 17:27:31 +1000

    cashRegister COMPLETE, can be improved though

--- cashRegister.js
+++ cashRegister.js
@@ -118,6 +118,7 @@ function checkCashRegister(price, cash, cid) {
   function findChange(){
     //console.log(changeInt)
     for (let i = cid.length - 1; i >= 0; i--) { //iterate descendingly through cid to compare largest to smallest bills
+      console.log(changeInt)
       if (cidMult[cid[i][0]][0] > 0 && changeInt >= Math.round(cidMult[cid[i][0]][1]*100)) {    //if there is money in till of that type and that it is less than or equal to the change
         console.log(i)
         //console.log(Math.round(cidMult[cid[i][0]][1]*100));
@@ -136,6 +137,12 @@ function checkCashRegister(price, cash, cid) {
           }
         }
       }
+      console.log(cidMult[cid[0][0]][0])
+      if (cidMult[cid[0][0]][0] === 0) {
+        statChange["status"] = "INSUFFICIENT_FUNDS"
+        console.log(statChange)
+        return statChange;
+      }
     }
   }
 
@@ -146,6 +153,9 @@ function checkCashRegister(price, cash, cid) {
     return statChange["change"].indexOf(value) ===index;
   })
   statChange["change"] = filterChange
+  if (statChange["status"] == "INSUFFICIENT_FUNDS") {
+    statChange["change"] = []
+  }
 
   console.log(filterChange)
   console.log(statChange)
@@ -156,4 +166,4 @@ function checkCashRegister(price, cash, cid) {
   return statChange;
 }
 
-checkCashRegister(3.26, 100, [["PENNY", 1.01], ["NICKEL", 2.05], ["DIME", 3.1], ["QUARTER", 4.25], ["ONE", 90], ["FIVE", 55], ["TEN", 20], ["TWENTY", 60], ["ONE HUNDRED", 100]])
+checkCashRegister(19.5, 20, [["PENNY", 0.01], ["NICKEL", 0], ["DIME", 0], ["QUARTER", 0], ["ONE", 1], ["FIVE", 0], ["TEN", 0], ["TWENTY", 0], ["ONE HUNDRED", 0]])
